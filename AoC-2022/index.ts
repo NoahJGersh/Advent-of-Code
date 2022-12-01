cc54bef4d381cdd6d6e77fa8e3500242b581dd6a
Noah Gersh <me@noahger.sh>
2022-12-01 13:33:16 -0800

    Initial commit.

---
+++ AoC-2022/index.ts
@@ -0,0 +1,22 @@
+// AoC 2022 - index.ts
+const src = './src';
+const fs = require('fs');
+
+function runAoC() {
+  const args = process.argv;
+  if (args.length !== 3) {
+    console.log('Usage: node index.ts <day number>');
+    process.exit();
+  }
+
+  const days = fs.readdirSync(src).length;
+  if (args[2] > days) {
+    console.error(`Error: <day> must be between 1 and ${days} (inclusive)`);
+    process.exit();
+  }
+
+
+  const day = require(`${src}/day${args[2]}`);
+}
+
+runAoC();
